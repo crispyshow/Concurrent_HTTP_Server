8a78579716cc92b61bdf6349c5458418241f470f
crispyshow <whytom2@163.com>
2016-03-07 22:40:25 +0800

    Applied executorService class to support concurrency

--- src/com/company/ConcurrentHttpServer.java
+++ src/com/company/ConcurrentHttpServer.java
@@ -1,36 +1,59 @@
 package com.company;
 
 /* This project implements an concurrent HTTP server
-   The current version implements the first stage: a server socket
+   The current version implements the second stage: a concurrent server
    The current server responds to the client with system time information */
 
 import java.net.*;
 import java.io.*;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class ConcurrentHttpServer {
+
     public static int PORT = 2555;
 
-    public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(PORT)) {
-            while (true) {
-                try (Socket connection = server.accept()) {
-                    respondClientRequest(connection);
-                } catch (IOException ex) {
-                }
+    // This function respond to clients with system time information
+    private static class respondClientRequest implements Runnable {
+
+        private Socket connection;
+        public respondClientRequest(Socket connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public void run(){
+            try (OutputStream os = connection.getOutputStream();
+                OutputStreamWriter out = new OutputStreamWriter(os)){
+                Date today = new Date();
+                out.write(today.toString() + "\n");
+                out.flush();
+                connection.close();
+            }catch(IOException ex) {
+                System.err.println(ex);
             }
-        } catch (IOException ex) {
-            System.err.println(ex);
         }
     }
 
-    // This function respond to clients with system time information
-    private static void respondClientRequest(Socket connection) throws IOException {
-        OutputStream os = connection.getOutputStream();
-        OutputStreamWriter out = new OutputStreamWriter(os);
-        Date today = new Date();
-        out.write(today.toString() + "\n");
-        out.flush();
-        connection.close();
+    //The main function uses ExecutorService class to support concurrency
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                try (Socket connection = server.accept()) {
+                    Runnable task = new respondClientRequest(connection);
+                    pool.submit(task);
+
+                } catch (IOException ex) {
+                    System.err.println(ex);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }
